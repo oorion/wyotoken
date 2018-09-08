41313fb8ba63cccda29a65ff3f0ba749eb6a61a4
Gabriel Cardona <gabriel@bitcoin.com>
2018-09-08 13:28:57 -0600

    Remove unused code. Add onChange and onSubmit methods.

--- src/App.js
+++ src/App.js
@@ -7,142 +7,68 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 let BITBOXCli = require('bitbox-cli/lib/bitbox-cli').default;
 let BITBOX = new BITBOXCli();
 
-let langs = [
-  'english',
-  'chinese_simplified',
-  'chinese_traditional',
-  'korean',
-  'japanese',
-  'french',
-  'italian',
-  'spanish'
-]
-
-let lang = langs[Math.floor(Math.random()*langs.length)];
-
-// create 256 bit BIP39 mnemonic
-let mnemonic = BITBOX.Mnemonic.generate(256, BITBOX.Mnemonic.wordLists()[lang])
-
-// root seed buffer
-let rootSeed = BITBOX.Mnemonic.toSeed(mnemonic);
-
-// master HDNode
-let masterHDNode = BITBOX.HDNode.fromSeed(rootSeed, 'bitcoincash');
-
-// HDNode of BIP44 account
-let account = BITBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'");
-
-// derive the first external change address HDNode which is going to spend utxo
-let change = BITBOX.HDNode.derivePath(account, "0/0");
-
-// get the cash address
-let cashAddress = BITBOX.HDNode.toCashAddress(change);
-
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mnemonic: mnemonic,
-      lang: lang,
-      hex: '',
-      txid: ''
+      name: '',
+      category: '',
+      url: '',
+      description: '',
+      tokenManagementAddress: ''
     }
   }
 
   componentDidMount() {
-    BITBOX.Address.utxo(cashAddress).then((result) => {
-      if(!result[0]) {
-        return;
-      }
+  }
 
-      // instance of transaction builder
-      let transactionBuilder = new BITBOX.TransactionBuilder('bitcoincash');
-      // original amount of satoshis in vin
-      let originalAmount = result[0].satoshis;
+  handleSubmit(e) {
+    BITBOX.RawTransactions.getRawTransaction(this.state.txid).then((result) => {
+    }, (err) => { console.log(err); });
+    e.preventDefault();
+  }
 
-      // index of vout
-      let vout = result[0].vout;
-
-      // txid of vout
-      let txid = result[0].txid;
-
-      // add input with txid and index of vout
-      transactionBuilder.addInput(txid, vout);
-
-      // get byte count to calculate fee. paying 1 sat/byte
-      let byteCount = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: 1 });
-      // 192
-      // amount to send to receiver. It's the original amount - 1 sat/byte for tx size
-      let sendAmount = originalAmount - byteCount;
-
-      // add output w/ address and amount to send
-      transactionBuilder.addOutput(cashAddress, sendAmount);
-
-      // keypair
-      let keyPair = BITBOX.HDNode.toKeyPair(change);
-
-      // sign w/ HDNode
-      let redeemScript;
-      transactionBuilder.sign(0, keyPair, redeemScript, transactionBuilder.hashTypes.SIGHASH_ALL, originalAmount);
-
-      // build tx
-      let tx = transactionBuilder.build();
-      // output rawhex
-      let hex = tx.toHex();
-      this.setState({
-        hex: hex
-      });
-
-      // sendRawTransaction to running BCH node
-      BITBOX.RawTransactions.sendRawTransaction(hex).then((result) => {
-        this.setState({
-          txid: result
-        });
-      }, (err) => {
-        console.log(err);
-      });
-    }, (err) => {
-      console.log(err);
-    });
+  handleInputChange(e) {
+    let value = e.target.value;
+    let id = e.target.id;
+    let obj = {};
+    obj[id] = value;
+    this.setState(obj);
   }
 
   render() {
-    let addresses = [];
-    for(let i = 0; i < 10; i++) {
-      let account = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`);
-      addresses.push(<li key={i}>m/44&rsquo;/145&rsquo;/0&rsquo;/0/{i}: {BITBOX.HDNode.toCashAddress(account)}</li>);
-    }
     return (
       <Container>
         <Row>
           <Col className='header'>
             <img src={logo} className="App-logo" alt="logo" />
+          </Col>
+          <Col className='header'>
             <h1>WyoToken Utility Token Generator</h1>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Form>
+            <Form onSubmit={this.handleSubmit.bind(this)}>
               <FormGroup>
                 <Label for="name">Name</Label>
-                <Input type="text" name="name" id="name" placeholder="Name" />
+                <Input onChange={this.handleInputChange.bind(this)} type="text" name="name" id="name" placeholder="Name" value={this.state.name} />
               </FormGroup>
               <FormGroup>
                 <Label for="category">Category</Label>
-                <Input type="text" name="category" id="category" placeholder="Category" />
+                <Input onChange={this.handleInputChange.bind(this)} type="text" name="category" id="category" placeholder="Category" value={this.state.category} />
               </FormGroup>
               <FormGroup>
                 <Label for="url">Url</Label>
-                <Input type="text" name="url" id="url" placeholder="Url" />
+                <Input onChange={this.handleInputChange.bind(this)} type="text" name="url" id="url" placeholder="Url" value={this.state.url} />
               </FormGroup>
               <FormGroup>
                 <Label for="description">Description</Label>
-                <Input type="text" name="description" id="description" placeholder="Description" />
+                <Input  onChange={this.handleInputChange.bind(this)} type="text" name="description" id="description" placeholder="Description" value={this.state.description}/>
               </FormGroup>
               <FormGroup>
                 <Label for="token-management-address">Token Management Address</Label>
-                <Input type="text" name="token-management-address" id="token-management-address" placeholder="Token Management Address" />
+                <Input onChange={this.handleInputChange.bind(this)} type="text" name="tokenManagementAddress" id="tokenManagementAddress" placeholder="Token Management Address" value={this.state.tokenManagementAddress} />
               </FormGroup>
               <Button>Submit</Button>
             </Form>
