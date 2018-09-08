c84de1b994c7a7c7593491c311b0ed8a6d3accc2
orion <orion.osborn@gmail.com>
2018-09-08 12:40:50 -0600

    Add form to page

--- src/App.js
+++ src/App.js
@@ -124,71 +124,24 @@ class App extends Component {
           <Col>
             <Form>
               <FormGroup>
-                <Label for="exampleEmail">Email</Label>
-                <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                <Label for="name">Name</Label>
+                <Input type="text" name="name" id="name" placeholder="Name" />
               </FormGroup>
               <FormGroup>
-                <Label for="examplePassword">Password</Label>
-                <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                <Label for="category">Category</Label>
+                <Input type="text" name="category" id="category" placeholder="Category" />
               </FormGroup>
               <FormGroup>
-                <Label for="exampleSelect">Select</Label>
-                <Input type="select" name="select" id="exampleSelect">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Input>
+                <Label for="url">Url</Label>
+                <Input type="text" name="url" id="url" placeholder="Url" />
               </FormGroup>
               <FormGroup>
-                <Label for="exampleSelectMulti">Select Multiple</Label>
-                <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Input>
+                <Label for="description">Description</Label>
+                <Input type="text" name="description" id="description" placeholder="Description" />
               </FormGroup>
               <FormGroup>
-                <Label for="exampleText">Text Area</Label>
-                <Input type="textarea" name="text" id="exampleText" />
-              </FormGroup>
-              <FormGroup>
-                <Label for="exampleFile">File</Label>
-                <Input type="file" name="file" id="exampleFile" />
-                <FormText color="muted">
-                  This is some placeholder block-level help text for the above input.
-                  It's a bit lighter and easily wraps to a new line.
-                </FormText>
-              </FormGroup>
-              <FormGroup tag="fieldset">
-                <legend>Radio Buttons</legend>
-                <FormGroup check>
-                  <Label check>
-                    <Input type="radio" name="radio1" />{' '}
-                    Option one is this and that—be sure to include why it's great
-                  </Label>
-                </FormGroup>
-                <FormGroup check>
-                  <Label check>
-                    <Input type="radio" name="radio1" />{' '}
-                    Option two can be something else and selecting it will deselect option one
-                  </Label>
-                </FormGroup>
-                <FormGroup check disabled>
-                  <Label check>
-                    <Input type="radio" name="radio1" disabled />{' '}
-                    Option three is disabled
-                  </Label>
-                </FormGroup>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="checkbox" />{' '}
-                  Check me out
-                </Label>
+                <Label for="token-management-address">Token Management Address</Label>
+                <Input type="text" name="token-management-address" id="token-management-address" placeholder="Token Management Address" />
               </FormGroup>
               <Button>Submit</Button>
             </Form>
