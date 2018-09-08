b9ebf0d681262514a8fa6f44396341a91f2a703c
Gabriel Cardona <gabriel@bitcoin.com>
2018-09-08 15:25:33 -0600

    Hardcode mnemonic and create managed token.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import logo from './logo.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 let BITBOXCli = require('bitbox-cli/lib/bitbox-cli').default;
-let BITBOX = new BITBOXCli();
+let BITBOX = new BITBOXCli({
+  restUrl: 'https://trest.bitcoin.com/v1/'
+});
+let wormhole = require("wormholecash/lib/Wormhole").default;
+let Wormhole = new wormhole({
+  restURL: 'https://wormholecash-staging.herokuapp.com/v1/'
+});
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +19,7 @@ class App extends Component {
     this.state = {
       name: '',
       category: '',
+      subcategory: 'subcategory',
       url: '',
       description: '',
       tokenManagementAddress: '',
@@ -24,10 +31,51 @@ class App extends Component {
   componentDidMount() {
   }
 
-  handleSubmit(e) {
+  async handleSubmit(e) {
+    e.preventDefault();
     // BITBOX.RawTransactions.getRawTransaction(this.state.txid).then((result) => {
     // }, (err) => { console.log(err); });
-    // e.preventDefault();
+    let mnemonic = 'remain bring turkey race next ivory direct crunch dwarf material correct oak intact copper jaguar lottery hope under kingdom robot leopard call giggle genuine';
+
+    // root seed buffer
+    let rootSeed = BITBOX.Mnemonic.toSeed(mnemonic);
+
+    // master HDNode
+    let masterHDNode = BITBOX.HDNode.fromSeed(rootSeed, 'testnet');
+
+    // HDNode of BIP44 account
+    let account = BITBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'");
+
+    // derive the first external change address HDNode which is going to spend utxo
+    let change = BITBOX.HDNode.derivePath(account, "0/0");
+
+    // get the cash address
+    let cashAddress = BITBOX.HDNode.toCashAddress(change);
+
+    let managed = await Wormhole.PayloadCreation.managed(1, 0, 0, this.state.category, this.state.subcategory, this.state.name, this.state.url, this.state.description);
+    let utxo = [{
+      txid: "0a6a946cc8b9d969dd482f13215d7da1eaf98518079e96cdc7829850d60c841a",
+      vout: 1,
+      scriptPubKey: "76a91423da806c2dbf8f7381c391d1018cec0f963d491888ac",
+      amount: 0.02,
+      value: 0.02,
+      satoshis: 2000000
+    }];
+
+    let rawTx = await Wormhole.RawTransactions.create(utxo, {});
+    let opReturn = await Wormhole.RawTransactions.opReturn(rawTx, managed);
+    let ref = await Wormhole.RawTransactions.reference(opReturn, cashAddress);
+    let changeHex = await Wormhole.RawTransactions.change(ref, utxo, cashAddress, 0.006);
+
+    let tx = Wormhole.Transaction.fromHex(changeHex)
+    let tb = Wormhole.Transaction.fromTransaction(tx)
+
+    let keyPair = Wormhole.HDNode.toKeyPair(change);
+    let redeemScript;
+    tb.sign(0, keyPair, redeemScript, 0x01, utxo[0].satoshis);
+    let builtTx = tb.build()
+    let txHex = builtTx.toHex();
+    let txid = await BITBOX.RawTransactions.sendRawTransaction(txHex);
     this.setState({
       showForm: false,
       showSuccess: true
@@ -96,7 +144,7 @@ class App extends Component {
           </Col>
         </Row>
         {formMarkup}
-        {successMarkup}
+        {successMarkup  }
       </Container>
     );
   }
