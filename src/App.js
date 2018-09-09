16acc41b3648ad6aee1cfecd11b7afbb08b2acc6
Gabriel Cardona <gabriel@bitcoin.com>
2018-09-08 18:22:34 -0600

    CSS

--- src/App.js
+++ src/App.js
@@ -153,23 +153,23 @@ class App extends Component {
             <Form onSubmit={this.handleSubmit.bind(this)}>
               <FormGroup>
                 <Label for="name">Name</Label>
-                <Input onChange={this.handleInputChange.bind(this)} type="text" name="name" id="name" placeholder="Name" value={this.state.name} />
+                <Input className='inputField' onChange={this.handleInputChange.bind(this)} type="text" name="name" id="name" placeholder="Name" value={this.state.name} />
               </FormGroup>
               <FormGroup>
                 <Label for="category">Category</Label>
-                <Input onChange={this.handleInputChange.bind(this)} type="text" name="category" id="category" placeholder="Category" value={this.state.category} />
+                <Input className='inputField' onChange={this.handleInputChange.bind(this)} type="text" name="category" id="category" placeholder="Category" value={this.state.category} />
               </FormGroup>
               <FormGroup>
                 <Label for="url">Url</Label>
-                <Input onChange={this.handleInputChange.bind(this)} type="text" name="url" id="url" placeholder="Url" value={this.state.url} />
+                <Input className='inputField' onChange={this.handleInputChange.bind(this)} type="text" name="url" id="url" placeholder="Url" value={this.state.url} />
               </FormGroup>
               <FormGroup>
                 <Label for="description">Description</Label>
-                <Input  onChange={this.handleInputChange.bind(this)} type="text" name="description" id="description" placeholder="Description" value={this.state.description}/>
+                <Input className='inputField'  onChange={this.handleInputChange.bind(this)} type="text" name="description" id="description" placeholder="Description" value={this.state.description}/>
               </FormGroup>
               <FormGroup>
                 <Label for="token-management-address">Token Management Address</Label>
-                <Input onChange={this.handleInputChange.bind(this)} type="text" name="tokenManagementAddress" id="tokenManagementAddress" placeholder="Token Management Address" value={this.state.tokenManagementAddress} />
+                <Input className='inputField' onChange={this.handleInputChange.bind(this)} type="text" name="tokenManagementAddress" id="tokenManagementAddress" placeholder="Token Management Address" value={this.state.tokenManagementAddress} />
               </FormGroup>
               <Button>Submit</Button>
             </Form>
