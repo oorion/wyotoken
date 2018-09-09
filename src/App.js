2f8e1de1abe9675ced2be3526a0d87ca74ef7904
orion <orion.osborn@gmail.com>
2018-09-09 09:46:08 -0600

    remove duplicate step 2 and 3

--- src/App.js
+++ src/App.js
@@ -188,13 +188,7 @@ class App extends Component {
               <h2 className='stepHeaders'>Step 2: Send $.01 (miners fee)</h2>
             </Col>
           </Row>
-          <Row className='payment'>
-          <Col>
-              <h2 className='stepHeaders'> Step 2: Send $.01 (miners fee) </h2>
-            </Col>
-          </Row>
           <Row className='center'>
-
             <Col>
               <QRCode value={cashAddress} className='qrcode' />
             </Col>
@@ -215,7 +209,6 @@ class App extends Component {
       managementMarkup.push(
         <Row>
           <Col>
-            <h2 className ='stepHeaders'>Step 3: Grant Tokens</h2>
             <h2 className='stepHeaders'>Step 3: Grant Tokens</h2>
             <Form onSubmit={this.handleTokenCreation.bind(this)}>
               <FormGroup>
